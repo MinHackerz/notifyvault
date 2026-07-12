59d8ff4737643c3cfa2cccf5e83374755fae96e7
Menajul Hoque <menajulhoque99@gmail.com>
2026-07-13 01:42:22 +0530

    Support dynamic test ad unit ID in debug mode

--- lib/core/helpers/ad_helper.dart
+++ lib/core/helpers/ad_helper.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 
 /// Helper class to supply Ad Unit IDs.
 class AdHelper {
@@ -7,7 +8,11 @@ class AdHelper {
   /// Returns the appropriate Banner Ad Unit ID based on platform.
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      // Standard Android Test Banner ID
+      if (kDebugMode) {
+        // Official Google Android Test Banner ID
+        return 'ca-app-pub-3940256099942544/6300978111';
+      }
+      // Production Android Banner ID
       return 'ca-app-pub-6501509019152141/6856469801';
     } else if (Platform.isIOS) {
       // Standard iOS Test Banner ID
